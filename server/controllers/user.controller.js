5659dcdccfd1fff413ec3a73501b2dc6eca80e40
princesinghrajput <psr8084@gmail.com>
2025-03-08 12:56:52 +0530

    chore: add validation

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -30,6 +30,15 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()){
+    return res.status(400).json({
+      errors: errors.array(),
+    })
+  }
+
+
   try {
     const user = await getUserByEmail(email);
 
